684dd5ac4f396f22dec0320f2df49c6a220b004e
grahammcbain@gmail.com <[grahammcbain@gmail.com]>
2014-07-09 09:56:02 -0600

    uhh wut

---
+++ lib/dog_hash.rb
@@ -0,0 +1,4 @@
+DOG + {
+  :cattle_dog => {:preferences => [{:name => "Harleigh"}], :first_name => 'Harleigh', :color => 'brown', :names => ['Harleigh', 'brown']},
+  "harleigh" => {:preferences => [{}]}
+}
